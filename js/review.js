6c79546a2a5754dc468df13d85e583e58d4c002b
dev-rjw <rjw0105@gmail.com>
2024-08-02 21:22:37 +0900

    feat: 리뷰 가져오기 기능 추가

--- js/review.js
+++ js/review.js
@@ -13,3 +13,28 @@ function setReview() {
 
     getReview();
 }
+
+// 리뷰 가져오기
+function getReview() {
+    if (localStorage.length > 0) {
+        reviewList = [...JSON.parse(localStorage.getItem("review"))];
+    }
+    const reviewBox = document.querySelector(".reviewBox");
+    reviewBox.innerHTML = "";
+    for (let i = 0; i < reviewList.length; i++) {
+        let tempHtml = `
+            <div>
+                <div>${reviewList[i]["user"]}</div>
+                <div>${reviewList[i]["review"]}</div>
+                <button>수정</button>
+                <button>삭제</button>
+            </div>
+            <hr>
+        `;
+        reviewBox.innerHTML += tempHtml;
+    }
+}
+
+window.onload = () => {
+    getReview();
+};
