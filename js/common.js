0677653d8b1ae678e05695216e97c6c86671aff8
Taeyeon <lty0314@gmail.com>
2024-08-02 16:27:41 +0900

    feat: 조건별 영화 리스트 정렬 기능 추가(별점순, 이름순(ko/en), 개봉일순)

--- js/common.js
+++ js/common.js
@@ -1,14 +1,17 @@
 import { getTopLated } from './API.js';
 import { formattedMovieData } from './formatMovie.js';
 
-const movieLists = [];
+let movieLists = [];
+let filteredMovies = [];
 
 // 영화 데이터 가져오기
 const fetchMovieData = async () => {
   try {
     const { results: movies } = await getTopLated('top_rated');
 
-    movieLists.push(...formattedMovieData(movies))
+    // movieLists.push(...formattedMovieData(movies))
+    movieLists = formattedMovieData(movies);
+    filteredMovies = [...movieLists];
 
     return movieLists;
   } catch (err) {
@@ -23,10 +26,14 @@ const createMovieCards = async (filteredMovies = null) => {
   const movieLists = filteredMovies || await fetchMovieData();
 
 
+
   const ul = document.querySelector('.movieCards');
   ul.innerHTML = '';
 
+
   movieLists.map(movie => {
+    const { id, koTitle, enTitle, imgUrl, overview, rating, date } = movie
+
     const { id, koTitle, enTitle, imgUrl, overview, rating, date } = movie
 
     const li = document.createElement('li');
@@ -53,7 +60,12 @@ const createMovieCards = async (filteredMovies = null) => {
         </div>
       </a>
     `;
+    li.addEventListener('click', () => {
+      alert('영화 id: ' + id);
+    });
+
     ul.appendChild(li);
+
 
   })
 }
@@ -64,7 +76,11 @@ const form = document.getElementById('searchForm');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
+
   const input = document.getElementById('search').value.trim()
+
+  input.length
+    ? createMovieCards(filterNames(input))
 
   input.length
     ? createMovieCards(filterNames(input))
@@ -81,6 +97,48 @@ const filterNames = (input) => {
       return check
     })
     : []
+
+  return value
+    ? movieLists.filter(movie => {
+      const check = movie.koTitle.includes(value) || movie.enTitle.includes(value);
+      return check
+    })
+    : []
 }
 
-createMovieCards();
+document.addEventListener('DOMContentLoaded', async () => {
+  await fetchMovieData();
+  createMovieCards();
+
+  const selectElement = document.getElementById('selectFilterMovie');
+
+  selectElement.addEventListener('change', (event) => {
+    const filterValue = event.target.value;
+
+    // console.log(fetchMovieData());
+    // 별점순
+    if (filterValue === 'star') {
+      filteredMovies = movieLists.slice().sort(function (a, b) {
+        return b.rating - a.rating;
+      });
+      // 이름순(ko) 정렬
+    } else if (filterValue === 'ko') {
+      filteredMovies = movieLists.slice().sort(function (a, b) {
+        return a.koTitle.localeCompare(b.koTitle);
+      });
+      // 이름순(en) 정렬
+    } else if (filterValue === 'en') {
+      filteredMovies = movieLists.slice().sort(function (a, b) {
+        return a.enTitle.localeCompare(b.enTitle);
+      });
+      // 개봉일순 정렬
+    } else if (filterValue === 'releaseDate') {
+      filteredMovies = movieLists.slice().sort(function (a, b) {
+        return new Date(b.date) - new Date(a.date);
+      });
+    }
+    // console.log(filteredMovies);
+    // console.log(movieLists);
+    createMovieCards(filteredMovies);
+  });
+});
