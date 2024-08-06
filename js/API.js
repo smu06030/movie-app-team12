d2eb376531c0059e0c2aa226953285d6548e6cae
incheol <smu06030>
2024-08-06 14:48:36 +0900

    feat: dark

--- js/API.js
+++ js/API.js
@@ -27,7 +27,7 @@ const getTopLated = async (type) => {
   try {
     const response = await fetch(url, options);
     const json = await response.json();
-
+    console.log(json)
     return json;
   } catch (err) {
     console.error(err);
